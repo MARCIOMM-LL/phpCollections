367eec10c8235782a160243f399eacb889d6f78a
MARCIOMM-LL <32685719+MARCIOMM-LL@users.noreply.github.com>
2020-03-23 12:26:17 -0300

    Alterados

---
+++ TocadorDeMusica.php
@@ -0,0 +1,73 @@
+<?php
+
+class TocadorDeMusica {
+
+    private $musicas;
+
+    public function __construct(){
+        $this->musicas = new SplDoublyLinkedList();
+        #Retorna a posição inicial
+        $this->musicas->rewind();
+    }
+
+    public function adicionarMusicas(SplFixedArray $musicas){
+        for($musicas->rewind(); $musicas->valid(); $musicas->next()){
+            $this->musicas->push($musicas->current());
+        }
+
+        #Retorna a posição inicial
+        $this->musicas->rewind();
+    }
+
+    public function tocarMusica(){
+        if($this->musicas->count() === 0){
+            echo "Erro, nenhuma música no tocador";
+        }else{
+            echo "Tocando música: " . $this->musicas->current() . "<br>";
+        }
+    }
+
+    public function adicionarMusica($musica){
+        $this->musicas->push($musica);
+    }
+
+    public function avancarMusica(){
+        $this->musicas->next();
+
+        if(!$this->musicas->valid()){
+            $this->musicas->rewind();
+        }
+    }
+
+    public function voltarMusica(){
+        $this->musicas->prev();
+
+        if(!$this->musicas->valid()){
+            $this->musicas->rewind();
+        }
+    }
+
+    public function exibirMusicas(){
+        for($this->musicas->rewind(); $this->musicas->valid(); $this->musicas->next()){
+            echo "Música: " . $this->musicas->current() . "<br>";
+        }
+    }
+
+    public function exibirQuantidadeMusicas(){
+        echo "Existem " . $this->musicas->count() . " músicas no tocador.";
+    }
+
+    public function adicionarMusicaNoComecoDaPlaylist($musica){
+        $this->musicas->unshift($musica);
+    }
+
+    public function removerMusicaDoComecoDaPlaylist(){
+        $this->musicas->shift();
+    }
+
+    public function removerMusicaDoFinalDaPlaylist(){
+        $this->musicas->pop();
+    }
+}
+
+?>
--- aula1.php
+++ aula1.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'TocadorDeMusica.php';
+
 $musicas = new SplFixedArray(2);
 
 $musicas[0] = "One Dance";
@@ -10,6 +12,29 @@ $musicas->setSize(4);
 $musicas[2] = "Rockstar";
 $musicas[3] = "Love Yourself";
 
-echo $musicas->getSize();
+
+//Serve para ver a quantidade de arrays
+//echo $musicas->getSize();
+
+$tocador = new TocadorDeMusica();
+
+$tocador->adicionarMusicas($musicas);
+
+#$tocador->tocarMusica();
+
+#$tocador->adicionarMusica("Havana"); 
+$tocador->adicionarMusicaNoComecoDaPlaylist("Havana");
+
+$tocador->removerMusicaDoComecoDaPlaylist();
+
+$tocador->removerMusicaDoFinalDaPlaylist();
+
+#$tocador->avancarMusica();
+
+#$tocador->tocarMusica();
+
+$tocador->exibirMusicas();
+
+#$tocador->exibirQuantidadeMusicas();
 
 ?>
